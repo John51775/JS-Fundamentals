6e1edd7323c0dbadf9325c5f6dde50017a32a2cf
John Ngugi <ngaruiyajohn86@gmail.com>
2025-11-08 14:38:56 +0300

    Write a script that prints 3 lines

---
+++ 1-multi_languages.js
@@ -0,0 +1,19 @@
+/* let languages = ["C is fun", "Python is cool", "JavaScript is amazing"]
+console.log(languages[0]);
+console.log(languages[1]);
+console.log(languages[2]);
+
+const languages = ["C is fun", "Python is cool", "JavaScript is amazing"];
+
+for (let i=0; i < languages.length; i++){
+
+    console.log(languages[i]);
+}
+
+*/
+
+const languages = ["C is fun", "Python is cool", "JavaScript is amazing"];
+
+languages.forEach(message =>console.log(message));
+
+
