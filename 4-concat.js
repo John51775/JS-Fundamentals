9f707241be12ad6e9ec59b902bd67b1eb33a46d2
John Ngugi <ngaruiyajohn86@gmail.com>
2025-11-08 16:10:23 +0300

    create 4-concat.js

---
+++ 4-concat.js
@@ -0,0 +1,13 @@
+const args = process.argv.slice(2);
+
+if (args[0]!== undefined && args[1] !== undefined){
+
+    console.log(args[0] + " is " + args[1] );
+}
+
+else if (args[0] !== undefined && args[1]=== undefined){
+
+    console.log(args[0] + " is undefined" );
+}
+
+else {console.log("undefined is undefined");}
