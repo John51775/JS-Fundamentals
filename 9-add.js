c5efe74eb5c210675be7e6c1049c5aa2a9435b9d
John Ngugi <ngaruiyajohn86@gmail.com>
2025-11-08 18:07:38 +0300

    Write a script that prints the addition of 2 integers

---
+++ 9-add.js
@@ -0,0 +1,12 @@
+const args = process.argv.slice(2)
+const a =parseInt(args[0]);
+const b =parseInt(args[1]);
+
+if (isNaN(a) || isNaN(b) ) {
+
+    console.log("NaN");
+}else{
+
+function add(a, b){return a + b;}
+console.log(add(a, b)); 
+}
